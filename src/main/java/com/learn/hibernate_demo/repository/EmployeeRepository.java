43f33984059d883d4b1c399cc50e14bff300827e
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2024-06-09 15:16:00 +0530

    project set up completed

---
+++ src/main/java/com/learn/hibernate_demo/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.learn.hibernate_demo.repository;
+
+import com.learn.hibernate_demo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
